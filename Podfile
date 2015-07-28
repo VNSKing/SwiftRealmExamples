3e5bf67e156b279e4ff0b92f22e84ae55529b4c4
VNSKing <348554402@qq.com>
2015-07-28 09:59:22 +0800

    Create
    Create

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'SwiftRealmExamples' do
+  pod 'Alamofire'
+  pod 'RealmSwift'
+  pod 'SwiftyJSON'
+end
---
+++ Pods/Target Support Files/Pods-SwiftRealmExamples/Pods-SwiftRealmExamples-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftRealmExamples : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftRealmExamples
+@end
