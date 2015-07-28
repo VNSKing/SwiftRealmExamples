739d25786a5e5b9af43b84ed8fbadeee998e5b60
VNSKing <348554402@qq.com>
2015-07-28 09:11:55 +0800

    Initial Commit

---
+++ SwiftRealmExamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftRealmExamples
+//
+//  Created by king on 15/7/28.
+//  Copyright (c) 2015年 king. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
